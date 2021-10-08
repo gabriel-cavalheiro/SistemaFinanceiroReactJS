776863f62acc73c2b4a57c992dc01a669bb452e0
Gabriel Cavalheiro <gabriel.cavalheiromr@gmail.com>
2021-10-08 17:49:35 -0300

    Criação da arquitetura do dados

---
+++ src/data/categories.ts
@@ -0,0 +1,10 @@
+import { Category } from '../types/Category';
+
+
+
+
+export const categories: Category = {
+    food: {title: 'Alimentação', color: 'blue', expense: true},
+    rent: {title: 'Aluguel', color: 'brown', expense: true},
+    salary: {title: 'Salário', color: 'green', expense: false},
+}
