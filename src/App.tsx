2d445c36a33e1c661a26930355dd554d92ac3cc4
Gabriel Cavalheiro <gabriel.cavalheiromr@gmail.com>
2021-10-10 15:27:01 -0300

    Construção do ambiente para exibição das listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 import { Category } from './types/Category';
 import { categories } from './data/categories';
 import { items } from './data/items';
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
 
 const App = () => {
+  
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() =>{
+    setFilteredList( filterListByMonth(list, currentMonth) )
+  },[list, currentMonth]);
+
   return (
+   
     <C.Container>
       <C.Header>
         <C.HeaderText> Sistema Financeiro </C.HeaderText>
@@ -16,7 +29,7 @@ const App = () => {
 
     {/* Área de inserção */}
 
-    {/* Área de itens */}
+      <TableArea list={filteredList} />
 
 
       </C.Body>
